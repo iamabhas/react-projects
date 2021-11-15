8f7bff75534622fed247deaf2b824c83aed087f4
Abhas K <67013564+iamabhas@users.noreply.github.com>
2021-11-15 09:32:11 +0545

    Create Movie.js

---
+++ ghibli-movie-watcher/Movie.js
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react";
+const Movie = ({
+  id,
+  title,
+  original_title,
+  image,
+  producer,
+  director,
+  release_date,
+  description,
+  removeMovie
+}) => {
+  const [readMore, setReadMore] = useState(false);
+  const [moreInfo, setMoreInfo] = useState(false);
+  return (
+    <article key={id}>
+      <h2>
+        {title} ( <span>{original_title} </span> ){" "}
+      </h2>
+      <img src={image} alt={title} />
+      <div className="buttons">
+        <a href="#" className="link">
+          ▶ Watch now
+        </a>
+        <button
+          className="btn"
+          onClick={() => {
+            setMoreInfo(!moreInfo);
+          }}
+        >
+          {moreInfo ? "Less info" : "More info"}
+        </button>
+        <button
+          className="btn not-btn"
+          onClick={() => {
+            removeMovie(id);
+          }}
+        >
+          Not intrested
+        </button>
+      </div>
+      {moreInfo && (
+        <section className="info">
+          <ul className="more-info">
+            <li>Release Date: {release_date}</li>
+            <li>Director: {director}</li>
+            <li>Producer: {producer}</li>
+          </ul>
+          <p className="movie-info">
+            {readMore ? description : `${description.substring(0, 100)}...`}
+            <button
+              className="read-more"
+              onClick={() => {
+                setReadMore(!readMore);
+              }}
+            >
+              {readMore ? "Show less" : "Read more"}
+            </button>
+          </p>
+        </section>
+      )}
+    </article>
+  );
+};
+
+export default Movie;
